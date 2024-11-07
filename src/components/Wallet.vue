40756a8cb80c2058a53c73615ab8386d8216aeab
Noureddine AMRANE <n.amrane@era2140.com>
2024-11-07 09:29:54 +0100

    INIT : tailwind

---
+++ src/components/Wallet.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main>
+        <div class="bg-gray-900 text-white p-6 rounded-lg shadow-lg">
+            <div class="flex justify-between items-center mb-4 gap-9">
+                <h2 class="text-lg font-bold">OKX</h2>
+                <button class="bg-gray-800 hover:bg-gray-700 px-4 py-2 rounded-md">
+                    Connect
+                </button>
+            </div>
+            <ul class="space-y-4">
+                <li class="hover:bg-gray-800 rounded-md p-2">
+                    <a href="#" class="block">Inscribe</a>
+                </li>
+
+                <li class="hover:bg-gray-800 rounded-md p-2">
+                    <a href="#" class="block">Sign PSBT</a>
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Wallet from "../components/Wallet.vue";
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Wallet></Wallet>
   </main>
 </template>
